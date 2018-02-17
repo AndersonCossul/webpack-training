ed6f1c287a5794a8cf325c789022d47a62c05cb7
Anderson Cossul <anderson_cossul@hotmail.com>
2018-02-17 20:43:29 -0200

    Webpack for handling css.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const autoprefixer = require('autoprefixer')
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map', // best source map for development. Rich and fast.
@@ -16,8 +17,39 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css/,
+        exclude: /node_modules/,
+        use: [
+          // important here it'll load from bottom to top, so first postcss-loader, css-loader, then style-loader!
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1, // 1 because we just run 1 loader to it, the postcss-loader, down there.
+              modules: true, // css-modules,
+              localIdentName: '[name]__[local]__[hash:base64:5]' // [local] is the component name
+            }
+          },
+          {
+            // this guy can turn sass on for example. Here it'll add auto prefixes for the css
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer({
+                  browsers: [
+                    "> 1%",
+                    "last 2 versions"
+                  ]
+                })
+              ]
+            }
+          }
+        ]
       }
     ]
   }
